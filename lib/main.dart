93fe7fd8f805779c7c73b51615c342a7da72ab18
taku820801 <takuchan19820801@yahoo.co.jp>
2022-06-29 00:42:15 +0900

    誕生日選択まで追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,49 @@
+
+import 'package:dayfrombirth/result.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       home: Scaffold(
         appBar: AppBar(
           title: const Text('my birthday'),
         ),
-        body: SingleChildScrollView(
+        body: home(),
+        ),
+      );
+  }
+}
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String? name;
+    DateTime? birthday;
+
+    return SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
+              Padding(
+                padding: EdgeInsets.all(20.0),
                 child: TextField(
-                  decoration: InputDecoration(
+                  onChanged: (newText){
+                    name = newText;
+                  },
+                  decoration: const InputDecoration(
                     labelText: '名前',
                     hintText: '名前',
                     icon: Icon(Icons.account_circle),
@@ -29,65 +51,27 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               ElevatedButton(onPressed: (
-                  ) {
-                showDatePicker(
+                  ) async {
+                birthday = await showDatePicker(
                     context: context,
                     initialDate: DateTime(DateTime.now().year-10),
                     firstDate: DateTime(DateTime.now().year-100),
                     lastDate: DateTime(DateTime.now().year),);
               }, child: new Text('誕生日選択'),),
-              ElevatedButton(onPressed: () {}, child: const Text('生まれてから何日目？'))
+
+              ElevatedButton(onPressed: () {
+                if (name != null && birthday != null) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>
+                        Result(name: name!, dayFromBirthday:DateTime.now().difference(birthday!).inDays)),
+                    );
+                  print(name);
+                  print(birthday);
+                }
+              }, child: const Text('生まれてから何日目？'))
             ],
-          ) ,
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
     );
   }
 }
